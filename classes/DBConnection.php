9392c225c67290e4851052450d0b2e968b6313cf
Savaliya-M <masavaliya183@gmail.com>
2023-05-04 03:49:12 +0530

    first commit

---
+++ classes/DBConnection.php
@@ -0,0 +1,60 @@
+<?php
+// if(!defined('DB_SERVER')){
+//     require_once("../initialize.php");
+// }
+// class DBConnection{
+
+//     private $host = DB_SERVER;
+//     private $username = DB_USERNAME;
+//     private $password = DB_PASSWORD;
+//     private $database = DB_NAME;
+    
+//     public $conn;
+    
+//     public function __construct(){
+
+//         if (!isset($this->conn)) {
+            
+//             $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
+//             $this->conn = new mysqli("localhost","root@localhost","","storage_db");
+            
+//             if (!$this->conn) {
+//                 echo 'Cannot connect to database server';
+//                 exit;
+//             }            
+//         }    
+        
+//     }
+//      public function __destruct(){
+//          $this->conn->close();
+//     }
+// }
+
+
+
+// if (!defined('DB_SERVER')) {
+//     require_once('../initialize.php');
+// }
+
+class DBConnection{
+    private $host = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $database = 'storage_db';
+    public $conn;
+    
+    public function __construct(){
+        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
+        
+        if (!$this->conn) {
+            echo 'Cannot connect to database server';
+            exit;
+        }
+    }
+    
+    public function __destruct(){
+        $this->conn->close();
+    }
+}
+
+?>
